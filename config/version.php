c1d93e26fbb7da7a5769143075265e48e75c1d3b
Øistein Sørensen <sorenso@gmail.com>
2012-11-09 10:51:36 +0100

    Editing versions.php

--- config/version.php
+++ config/version.php
@@ -1,22 +1,16 @@
 <?php
-define('APP_VERSION','2.6.0');
-if (_CORE_IS_LIVE) {
-    define('APP_REVISION',substr('$Revision: 3908 $',11,4));
+define('APP_VERSION','0.0.1');
+
+if (IS_LIVE) {
+	define('APP_REVISION',substr('$Revision: 1 $',11,4));
+
 } else {
-    // Try SVN version
-    $v = shell_exec("(svn info | grep Revision | cut -d' ' -f 2) 2>/dev/null");
-    $v = trim($v);
-    if (is_numeric($v)) {
-        define('APP_REVISION', $v);
-    } else {
-        // git-svn/git?
-        $v = shell_exec("git rev-parse HEAD 2>/dev/null");
-        $v = trim($v);
-        if ($v) {
-            define('APP_REVISION', $v);
-        } else {
-            // I've no clue
-            define('APP_REVISION', time());
-        }
-    }
+	$v = shell_exec("git rev-parse HEAD 2>/dev/null");
+	$v = trim($v);
+	if ($v) {
+		define('APP_REVISION', $v);
+	} else {
+		define('APP_REVISION', 'unknown';
+	}
+
 }
